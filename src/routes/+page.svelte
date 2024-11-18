254f515630f10654649b17ff011c92b4a3bbd3c8
Anne van den Berg <berg0175@hz.nl>
2024-11-18 14:08:33 +0100

    test push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,8 @@
 			<span class="inline-block">Home Page</span>
 		</h2>
 	</section>
+
+    <section class="ontainer w-full mx-auto mt-4">
+        <div class="basis-4/6 bg-green-700 rounded-lg p-10 lg:mr-8"></div>
+    </section>
 </div>

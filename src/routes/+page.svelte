db924b105786a98d5a125a9a4d9d69bc5d48339e
Anne van den Berg <berg0175@hz.nl>
2024-11-19 10:07:30 +0100

    home page, boxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../app.css";
+    import background from '$lib/assets/background-rabbit.png';
 </script>
 
 <div class="container w-full mx-auto">
@@ -9,7 +10,39 @@
 		</h2>
 	</section>
 
-    <section class="ontainer w-full mx-auto mt-4">
-        <div class="basis-4/6 bg-green-700 rounded-lg p-10 lg:mr-8"></div>
+    <!-- Flex container for black and green boxes -->
+    <section class="mt-4 flex justify-between items-start">
+        <!-- Black box with centered background image and 5px margin -->
+        <div 
+            class="bg-black flex items-center justify-center" 
+            style="width: 630px; height: 430px;">
+
+            <div 
+                class="bg-cover bg-no-repeat" 
+                style="
+                    background-image: url({background});
+                    background-size: contain;
+                    background-position: center;
+                    width: calc(100% - 10px);
+                    height: calc(100% - 10px);">
+            </div>
+        </div>
+        
+        <!-- Green box with articles -->
+        <div 
+            class="bg-light-green p-4 flex flex-col space-y-4" 
+            style="width: 330px; height: 590px;">
+
+            <!-- White boxes - articles -->
+            <div 
+                class="bg-white shadow-md" 
+                style="width: 300px; height: 250px;">
+            </div>
+
+            <div 
+                class="bg-white shadow-md" 
+                style="width: 300px; height: 250px;">
+            </div>
+        </div>
     </section>
 </div>

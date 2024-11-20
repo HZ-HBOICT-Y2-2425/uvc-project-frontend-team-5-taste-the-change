c31cc0d9d934be004aa33907ede546fc5acc490b
meike <mart0143@hz.nl>
2024-11-20 14:06:48 +0100

    Started setting up the basic recipe page & layout

---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+ 
+</script>
+
+<div class="container w-full mx-auto">
+  <div class="grid grid-cols-4">
+    <!-- Basic area for filter -->
+    <div class="bg-light-green p-4 flex flex-col space-y-4 w-[330px] h-[590px] col-span-1">
+      <h2 class="text-4xl">
+          <span class="inline-block p-2">Filters</span>
+      </h2>
+    </div>
+    <section class="mt-10">
+      <h2 class="text-4xl">
+        <span class="inline-block">Recipes</span>
+      </h2>
+    </section>  
+  </div>
+</div>

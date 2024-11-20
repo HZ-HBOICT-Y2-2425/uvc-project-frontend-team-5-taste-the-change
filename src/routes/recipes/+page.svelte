80ed3f8b776884746d7d61f2c1591ad9e92a4c90
meike <mart0143@hz.nl>
2024-11-20 21:27:17 +0100

    hard-coded basics of the recipes are implemented

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -2,18 +2,39 @@
  
 </script>
 
-<div class="container w-full mx-auto">
-  <div class="grid grid-cols-4">
-    <!-- Basic area for filter -->
-    <div class="bg-light-green p-4 flex flex-col space-y-4 w-[330px] h-[590px] col-span-1">
-      <h2 class="text-4xl">
-          <span class="inline-block p-2">Filters</span>
-      </h2>
+<div class="container w-full m-5">
+	<section class="mb-5">
+		<h2 class="text-6xl">
+			Recipes
+		</h2>
+	</section>
+  <section>
+    <div class="bg-indigo-300 w-[1150px] h-auto p-2">
+      <div class="bg-green-50 rounded-lg border-2 border-black h-48 mt-3 mb-3">
+        <div class="grid grid-cols-3">
+          <h3 class="text-xl p-3 col-span-2">Vegetarian Spaghetti</h3>
+          <p class="text-green-600 text-lg col-span-1 p-3">Suggestion for you</p>
+        </div>
+        <p class="text-sm w-10/12 p-3">
+          Indulge in a plate of vibrant, flavourful vegetarian spaghetti! 🍝 Imagine twirling your fork through perfectly cooked spaghetti, 
+          coated in a rich, homemade tomato sauce bursting with the freshness of ripe tomatoes, garlic, and basil. Toss in some sautéed bell 
+          peppers, zucchini, and mushrooms for a delightful medley of textures and tastes. Top it off with a sprinkle of Parmesan cheese and 
+          fresh herbs, and you’ve got a meal that’s not only satisfying but also a feast for the senses. Perfect for a cozy dinner or a special 
+          occasion, this vegetarian spaghetti will have everyone asking for seconds!
+        </p>
+      </div>   
+      <div class="bg-slate-50 rounded-lg border-2 border-black h-48 mt-3 mb-3">
+        <div>
+          <h3 class="text-xl p-3">Vegan Curry</h3>
+        </div>
+        <p class="text-sm w-10/12 p-3">
+          Dive into a bowl of vibrant, aromatic vegan curry! 🍛 Imagine tender chunks of vegetables simmered in a rich, 
+          creamy coconut milk base, infused with the warmth of spices like turmeric, cumin, and coriander. Each bite is a burst of flavour, 
+          with the sweetness of carrots, the earthiness of potatoes, and the slight crunch of bell peppers. Garnished with fresh cilantro and 
+          a squeeze of lime, this curry is not only a feast for the taste buds but also a nourishing delight. Perfect for a cozy night in 
+          or impressing guests, this vegan curry will become a staple in your kitchen!
+        </p>
+      </div>  
     </div>
-    <section class="mt-10">
-      <h2 class="text-4xl">
-        <span class="inline-block">Recipes</span>
-      </h2>
-    </section>  
-  </div>
+  </section>
 </div>

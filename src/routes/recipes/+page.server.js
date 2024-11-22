698385e7b5c2711fea7090ea8fde4fd664d5340b
RaineScarlett <araj0001@hz.nl>
2024-11-22 17:31:24 +0100

    fixed the error of not displaying recipes

--- src/routes/recipes/+page.server.js
+++ src/routes/recipes/+page.server.js
@@ -9,15 +9,16 @@ export async function load() {
         console.log('Response status:', response.status); // Log the status of the response
         
         if (!response.ok) {
-            throw new Error('Failed to fetch pets');
+            throw new Error('Failed to fetch recipes');
         }
 
-        const recipes = await response.json();
+        const data = await response.json();
+        const recipes = data.recipes;
         console.log('Fetched recipes:', recipes); // Log the fetched pets
 
         return { data: { recipes } }; // Ensure returning the correct data structure
     } catch (error) {
-        console.error('Error fetching pets:', error); // Log any errors
+        console.error('Error fetching recipes:', error); // Log any errors
         return {
             // @ts-ignore
             data: { error: error.message } // Return the error in the same structure

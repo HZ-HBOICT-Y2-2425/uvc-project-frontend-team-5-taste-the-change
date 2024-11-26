b7910cea072628480c4f5cf13ca27d91baaddada
Anne van den Berg <berg0175@hz.nl>
2024-11-26 14:49:08 +0100

    navbar new prototype styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,20 +4,20 @@
 </script>
 
 <div class="flex flex-col h-screen justify-between">
-	<nav class="bg-dark-green flex items-center justify-between px-4 py-3">
+	<nav class="bg-gradient-to-r from-light-green via-green-100 to-blue-200 flex items-center justify-between px-4 py-3">
 		<div class="flex items-center space-x-2">
 			<img src={logo} alt="Logo" class="h-20 w-20" />
-			<a href="/" class="p-4 text-4xl font-sans font-bold text-slate-50 hover:text-green-950">
+			<a href="/" class="p-4 text-4xl font-sans font-bold text-black hover:text-gray-500">
 				Taste The Change
 			</a>
 		</div>
 
 		<ul class="flex text-2xl space-x-6 list-none">
-			<li class="list-none"><a href="/leaderboard" class="text-white hover:text-green-950">Leaderboard</a></li>
-			<li class="list-none"><a href="/goals" class="text-white hover:text-green-950">Goals</a></li>
-			<li class="list-none"><a href="/statistics" class="text-white hover:text-green-950">Statistics</a></li>
-			<li class="list-none"><a href="/recipes" class="text-white hover:text-green-950">Recipes</a></li>
-			<li class="list-none"><a href="/profile" class="text-white hover:text-green-950">Profile</a></li>
+			<li class="list-none"><a href="/leaderboard" class="text-black hover:text-gray-500">Leaderboard</a></li>
+			<li class="list-none"><a href="/goals" class="text-black hover:text-gray-500">Goals</a></li>
+			<li class="list-none"><a href="/statistics" class="text-black hover:text-gray-500">Statistics</a></li>
+			<li class="list-none"><a href="/recipes" class="text-black hover:text-gray-500">Recipes</a></li>
+			<li class="list-none"><a href="/profile" class="text-black hover:text-gray-500">Profile</a></li>
 		</ul>
     </nav>
 

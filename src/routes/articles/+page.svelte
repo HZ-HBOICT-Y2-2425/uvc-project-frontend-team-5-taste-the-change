3a2e93d64ac4a3b4a831539ee9a913b9a525c340
Stjampot <naal0004@hz.nl>
2024-11-27 10:05:10 +0100

    displayed simple article list

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { setContext } from "svelte";
+  
+    // Use setContext if needed
+    setContext("apiReference", {
+      mainUrl: "http://localhost:3011",
+    });
+    // `data` is automatically passed from the `load` function in the server
+    export let data;
+</script>
+
+<div class="w-3/4">
+    {#if data.data.articles && data.data.articles.length > 0}
+      <ul>
+        {#each data.data.articles as articles}
+            <p>{articles.name}</p>
+            <p>{articles.info}</p>
+        {/each}
+      </ul>
+    {:else}
+      <p>No articles found</p>
+    {/if}
+  </div>

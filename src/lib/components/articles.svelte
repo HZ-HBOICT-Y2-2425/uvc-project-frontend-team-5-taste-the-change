9a22ed191f017cf33b1416412987f8d46f55931c
Anne van den Berg <berg0175@hz.nl>
2024-12-09 10:59:59 +0100

    articles components still dont work

--- src/lib/components/articles.svelte
+++ src/lib/components/articles.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import Article from '$lib/components/article.svelte';
-  
-    // Define the type of an article
-    type ArticleType = {
-      name: string;
-      imgUrl: string;
-      info: string;
-    };
-  
-    let articles: ArticleType[] = [];
-  
-    const apiUrl = `http://localhost:3011/articles`;
-  
-    onMount(async () => {
+  import { onMount } from 'svelte';
+  import Article from '$lib/components/article.svelte';
+
+  let articles: never[] =  [];
+  let error: string | null = null;
+
+  const apiUrl = `http://localhost:3011/articles`;
+
+  onMount(async () => {
+    try {
       const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch articles: ${response.statusText}`);
+      }
       const result = await response.json();
       articles = result.articles;
-    });
-  </script>
-  
+    } catch (err) {
+      if (err instanceof Error) {
+        error = err.message;
+      } else {
+        error = String(err);
+      }
+    } console.log(error);
+  });
+</script>

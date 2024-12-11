4fd5a23d43eb8ad6e700281d02f27a9062dbd31f
meike <mart0143@hz.nl>
2024-12-11 11:06:34 +0100

    Added placeholder graph

---
+++ src/routes/statistics/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import placeholderGraph from '$lib/assets/placeholderGraph.png'
+</script>
+
+<div class="grid grid-cols-3">
+  <section class="col-span-2">
+    <img src={placeholderGraph} alt="placeholder graph">
+  </section>
+</div>

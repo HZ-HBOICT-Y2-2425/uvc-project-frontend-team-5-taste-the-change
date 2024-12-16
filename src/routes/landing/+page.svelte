a70b6397ef4212b92eba8ca8a60978cbed9b4189
meike <mart0143@hz.nl>
2024-12-16 13:53:25 +0100

    started design (added title)

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<h1 class="bg-gradient-to-r from-[#F2FDE3] via-[#D9FDCA] to-[#6DA4FF] drop-shadow-md font-extrabold inline-block text-transparent bg-clip-text text-5xl">Taste the change</h1>

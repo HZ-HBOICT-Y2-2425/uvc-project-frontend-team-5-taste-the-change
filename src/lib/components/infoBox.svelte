3769a5f992c017a90ccf9825c293331331133d42
RaineScarlett <araj0001@hz.nl>
2024-12-17 11:41:07 +0100

    removed all console errors and unnecessary console logs

--- src/lib/components/infoBox.svelte
+++ src/lib/components/infoBox.svelte
@@ -1,6 +1,6 @@
 <div class="bg-white p-4 rounded-lg shadow-md w-1/3 text-center">
     <slot name="image"></slot>
-    <p><slot name="description"></slot></p>
+    <slot name="description"></slot>
     <button
         class="mt-2 bg-white border border-blue-500 text-blue-500 px-4 py-1 rounded-full"
         >Learn More</button

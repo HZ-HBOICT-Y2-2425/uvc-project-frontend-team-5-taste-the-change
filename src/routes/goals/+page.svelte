f68ded10b510ca319c639cb31889401b47130c78
Stjampot <naal0004@hz.nl>
2024-12-17 21:26:27 +0100

    finished goals page

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -7,6 +7,7 @@
   import { leafAmount } from "../../stores/leafStore.js";
   import { countdown, resetCountdown } from "../../stores/countdownStore.js";
    import { browser } from "$app/environment";
+  import InfoBox from "$lib/components/infoBox.svelte";
   
   export let data;
   let goals = [...data.data.goals]; // Local goals array
@@ -140,4 +141,24 @@
     </button>
   </div>
   <p class="mt-6 text-gray-800">Leaves Collected: <span class="font-bold">{$leafAmount}</span></p>
+  
+<div>
+  <h2 class="text-2xl font-bold mb-4">How to Use Your Leaves?</h2>
+  <div class="flex space-x-4">
+      <InfoBox>
+          <img slot="image" src="src/lib/assets/Frame 26.png" alt="unlock rewards" class="w-40 h-auto mx-auto mb-2">
+          <p slot="description">Exchange your leaves for exciting bonuses.</p>
+      </InfoBox>
+      <InfoBox>
+          <img slot="image" src="src/lib/assets/Frame 26 (1).png" alt="customize mascot" class="w-52 h-auto mx-auto mb-2">
+          <p slot="description">Customize the mascot with fun accessories and decorations.</p>
+      </InfoBox>
+      <InfoBox>
+          <img slot="image" src="src/lib/assets/Frame 26 (2).png" alt="show progress" class="w-52 h-auto mx-auto mb-2">
+          <p slot="description">Show off progress, climb leaderboards, and challenge friends.</p>
+      </InfoBox>
+  </div>
 </div>
+
+</div>
+

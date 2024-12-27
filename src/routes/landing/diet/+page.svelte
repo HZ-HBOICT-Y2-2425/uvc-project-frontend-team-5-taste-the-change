9b4baa8dd7ae9095754b05be0c4c28b1924150a3
meike <mart0143@hz.nl>
2024-12-27 16:28:47 +0100

    added button/selected styling

--- src/routes/landing/diet/+page.svelte
+++ src/routes/landing/diet/+page.svelte
@@ -5,31 +5,31 @@
       <p class="p-3">Which diet do you wish to achieve?</p>
     </section>
     <section class="p-10 grid grid-cols-3 gap-6">
-      <div class="bg-white m-3 text-center p-3">
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Vegetarian</h3>
         <p>You do not eat any meat product</p>
-      </div> 
-      <div class="bg-white m-3 text-center p-3">
+      </button> 
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Vegan</h3>
         <p>Your diet consist exclusively out of planed based meals, means no animal products.</p>
-      </div> 
-      <div class="bg-white m-3 text-center p-3">
+      </button> 
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Flexitarian</h3>
         <p>You follow mostly a meat-/fishless diet, but occasionally you eat meat and fish.</p>
-      </div> 
-      <div class="bg-white m-3 text-center p-3">
+      </button> 
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Pescatarian</h3>
         <p>Your diet consists of fish and no other meat.</p>
-      </div> 
-      <div class="bg-white m-3 text-center p-3">
+      </button> 
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Mediterranean</h3>
         <p>Your diet consist mainly on plant based products and is minimally proceeded. Proteins are gathered from leguminous plants (beans, nuts, seeds), 
           only occasionally animal products are consumed</p>
-      </div> 
-      <div class="bg-white m-3 text-center p-3">
+      </button> 
+      <button class="bg-white m-3 text-center p-3 focus:bg-[#d9fdca] rounded-2xl focus:shadow-sm focus:shadow-black">
         <h3 class="text-2xl pb-2 font-bold">Keto</h3>
         <p>You are following a high-fat and low carbohydrate dietary therapy  (medical reasons)</p>
-      </div>       
+      </button>       
     </section>
   </div>  
 </div>

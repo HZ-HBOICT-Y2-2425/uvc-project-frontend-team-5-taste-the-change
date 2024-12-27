f025317439271157c9ac6c087529a5c955ee4f9f
meike <mart0143@hz.nl>
2024-12-27 16:21:29 +0100

    basic styling finished

---
+++ src/routes/landing/diet/+page.svelte
@@ -0,0 +1,35 @@
+<div class="container w-full mx-auto">
+  <div class="relative w-full h-full">
+    <section class="pt-10 p-3 text-center">
+      <h1 class="text-5xl font-bold">Choose your diet goal</h1>
+      <p class="p-3">Which diet do you wish to achieve?</p>
+    </section>
+    <section class="p-10 grid grid-cols-3 gap-6">
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Vegetarian</h3>
+        <p>You do not eat any meat product</p>
+      </div> 
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Vegan</h3>
+        <p>Your diet consist exclusively out of planed based meals, means no animal products.</p>
+      </div> 
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Flexitarian</h3>
+        <p>You follow mostly a meat-/fishless diet, but occasionally you eat meat and fish.</p>
+      </div> 
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Pescatarian</h3>
+        <p>Your diet consists of fish and no other meat.</p>
+      </div> 
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Mediterranean</h3>
+        <p>Your diet consist mainly on plant based products and is minimally proceeded. Proteins are gathered from leguminous plants (beans, nuts, seeds), 
+          only occasionally animal products are consumed</p>
+      </div> 
+      <div class="bg-white m-3 text-center p-3">
+        <h3 class="text-2xl pb-2 font-bold">Keto</h3>
+        <p>You are following a high-fat and low carbohydrate dietary therapy  (medical reasons)</p>
+      </div>       
+    </section>
+  </div>  
+</div>

15f2ba8f55bde2ed47f9a0ed38ba29b06ef897fe
meike <mart0143@hz.nl>
2025-01-04 16:26:29 +0100

    implemented basic pages for changing certain account details such as username & diet goal

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -10,6 +10,24 @@
 
   import arrow from '$lib/assets/profile/arrow.png'
   import close from '$lib/assets/profile/close.png'
+
+  import { goto } from '$app/navigation';
+
+  function goToLandingPage() {
+    goto('/landing');
+  }
+
+  function goToChangePassword() {
+    goto('/profile/changePassword');
+  }
+
+  function goToChangeUsername() {
+    goto('/profile/changeUsername');
+  }
+
+  function goToChangeDietGoal() {
+    goto('/profile/changeDietGoal');
+  }
 </script>
 
 {#if showModal}
@@ -22,20 +40,20 @@
         </button>
       </section>
       <section>
-        <button class="m-2 p-2 flex items-center">
+        <button on:click={goToChangeUsername} class="m-2 p-2 flex items-center">
           <p>Change username</p>
           <img class="left-[180%] relative" src={arrow} alt="arrow">
         </button>
-        <button class="m-2 p-2 flex items-center">
+        <button on:click={goToChangePassword} class="m-2 p-2 flex items-center">
           <p>Change password</p>
           <img class="left-[184%] relative" src={arrow} alt="arrow">
         </button>
-        <button class="m-2 p-2 flex items-center">
+        <button on:click={goToChangeDietGoal} class="m-2 p-2 flex items-center">
           <p>Change diet goal</p>
           <img class="left-[193%] relative" src={arrow} alt="arrow">
         </button>
       </section>
-      <button class="text-[#FA5252] border-[#FA5252] border-2 rounded-xl flex items-center m-2 p-2 mt-5 relative left-[25%] bg-white">
+      <button on:click={goToLandingPage} class="text-[#FA5252] border-[#FA5252] border-2 rounded-xl flex items-center m-2 p-2 mt-5 relative left-[25%] bg-white">
         <p class="font-bold">Log out of your account</p>
       </button>
     </div>

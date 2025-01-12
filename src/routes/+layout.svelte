c7e55489bf0e4218e91c43eaa342274ca3902b05
alisiia02 <144117470+alisiia02@users.noreply.github.com>
2025-01-11 23:15:53 -0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,37 +15,36 @@
 
 <div class="flex flex-col h-screen">
 	<nav class="bg-gradient-to-r from-[#96DF77] via-[#D9FDCA] to-[#BDD4FA] flex items-center justify-between px-4 py-0.5">
-  <div class="flex items-center space-x-2">
-    <img src={logo} alt="Logo" class="h-14 w-14" />
-    <a href="/" class="p-3 text-3xl font-sans font-bold text-[#163957] hover:text-green-600 min-w-max">
-      Taste The Change
-    </a>
-  </div>
-  <div class="flex items-center w-full justify-end">
-    <ul class="flex items-center text-xl space-x-5 list-none">
-      <li class="list-none"><a href="/recipes" class="text-[#163957] hover:text-green-600">Recipes</a></li>
-      <li class="list-none"><a href="/goals" class="text-[#163957] hover:text-green-600">Goals</a></li>
-      <li class="list-none"><a href="/statistics" class="text-[#163957] hover:text-green-600">Statistics</a></li>
-      <li>
-        <a href="/profile">
-          <img src={profileicon} alt="profile icon" />
-        </a>
-      </li>
-    </ul>
-    <ul class="flex items-center text-xl list-none">
-      <li class="list-none"><img src="src/lib/assets/result (1) 6.png" alt="leaf"></li>
-      <li class="list-none">{$leafAmount}</li>
-    </ul>
-  </div>
-</nav>
-
-
+	<div class="flex items-center space-x-2">
+		<img src={logo} alt="Logo" class="h-14 w-14" />
+		<a href="/" class="p-3 text-3xl font-sans font-bold text-[#163957] hover:text-green-600 min-w-max">
+		Taste The Change
+		</a>
+	</div>
+	<div class="flex items-center w-full justify-end">
+		<ul class="flex items-center text-xl space-x-5 list-none">
+		<li class="list-none"><a href="/recipes" class="text-[#163957] hover:text-green-600">Recipes</a></li>
+		<li class="list-none"><a href="/goals" class="text-[#163957] hover:text-green-600">Goals</a></li>
+		<li class="list-none"><a href="/statistics" class="text-[#163957] hover:text-green-600">Statistics</a></li>
+		<li>
+			<a href="/profile">
+			<img src={profileicon} alt="profile icon" />
+			</a>
+		</li>
+		</ul>
+		<ul class="flex items-center text-xl list-none">
+		<li class="list-none"><img src="src/lib/assets/result (1) 6.png" alt="leaf"></li>
+		<li class="list-none">{$leafAmount}</li>
+		</ul>
+	</div>
+	</nav>
 
 	<main class="flex-grow">
+		<Breadcrumb />
 		<slot />
 	</main>
 
-	<footer class="bg-light-green">
-		<p class="p-2 text-center">V2.0</p>
+	<footer class="bg-gradient-to-r from-[#96DF77] via-[#D9FDCA] to-[#BDD4FA]">
+		<p class="p-2 text-center">2025 | Taste the Change - V2.0</p>
 	</footer>
 </div>

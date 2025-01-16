ccf667c4f4994cd94e1a5a263f0913284928b779
Anne van den Berg <berg0175@hz.nl>
2025-01-16 14:20:37 +0100

    unit testing for landing page works

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  test: {
+    environment: 'jsdom', // Change this to jsdom for browser-like testing
+    globals: true, // Enable Vitest global functions like describe and it
+    setupFiles: './src/setupTests.js', // Optional, for custom setup if needed
+  },
+  resolve: process.env.VITEST
+    ? {
+      conditions: ['browser'],
+    }
+    : undefined,
+  plugins: [sveltekit()],
 });

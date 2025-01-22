c6e4eee7d24cabdd5078a8c54a7970170d3c8b8f
Anne van den Berg <berg0175@hz.nl>
2025-01-22 12:36:02 +0100

    small changes

--- src/stores/leafStore.js
+++ src/stores/leafStore.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const leafAmount = writable(0); // Initialize with 0
+export const leafAmount = writable(20); // Initialize with 0
 
 export function incrementLeafAmount(value) {
   leafAmount.update(current => current + value); // Increment by the given value
